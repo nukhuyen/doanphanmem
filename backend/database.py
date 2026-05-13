bf801c28526a8edad3393d7dd3aaeb311a2b4f41
Y Nữ Khuyên <ynkhuyen.k23tt@kontum.udn.vn>
2026-05-13 11:24:33 +0700

    nah

--- backend/database.py
+++ backend/database.py
@@ -1,2 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
+import sys
+import os
+# Thêm dòng này để Python tìm thấy các file ở thư mục gốc
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from database import db
